450a83df855a0708d9b6e292820557c39316afaa
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-27 18:16:30 -0500

    Add migration for vote type.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,18 @@
 class VotesController < ApplicationController
 
   def create
+    question_or_answer(params)
+    @result.vote.create(user_id: current_user.id)
   end
 
   def destroy
+    @result.vote.
   end
 
   private
 
   def vote_params
+    params.require(:vote).permit()
   end
 
 end
