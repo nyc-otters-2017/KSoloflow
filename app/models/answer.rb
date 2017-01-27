e7107d17399d539c3ec278a138ec626a855b7bc5
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-27 11:52:14 -0500

    Add migration, model, and associations for comments.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
+  has_many :comments, as: :commentable
 
   validates :content, :user_id, :post_id, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   has_many :answers
+  has_many :comments, as: :commentable
 
   validates :user_id, :title, :content, presence: true
 end
