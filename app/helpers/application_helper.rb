ddbc9ac091671c752a14219fee96cc905531afe0
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-27 16:51:05 -0500

    Move helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def question_or_answer(params)
+    if params[:answer_id]
+      @answer = Answer.find(params[:answer_id])
+    else
+      @question = Question.find(params[:question_id])
+    end
+  end
+  
 end
