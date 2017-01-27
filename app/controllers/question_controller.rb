a7a579b6c4b77bfc132c7f338970b785936e7354
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-27 16:12:45 -0500

    Add logic to question controller.

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,25 +1,53 @@
 class QuestionController < ApplicationController
 
   def index
+    @questions = Question.all
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(question_params)
+    @question.user_id = current_user.id
+
+    if @question.save
+      redirect_to @question
+    else
+      render 'new'
+    end
   end
 
   def show
+    @question = Question.find(params[:id])
+    @answer = Answer.new
+    @comment = Comment.new
   end
 
   def edit
+    @question = Question.find(params[:id])
+    @user = @question.user
   end
 
   def update
+    @question = Question.find(params[:id])
+    @user = @question.user
+
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-  end
+    @question = Question.find(params[:id])
+      @user = @question.user
+        @question.destroy
+
+      redirect_to root_path
+    end
 
   private
 
