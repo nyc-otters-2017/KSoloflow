e2152700274138f31c0b67430827eaeac96ecf07
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-27 17:27:35 -0500

    Add content to comments controller.:

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,42 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment.new
+    question_or_answer(params)
+    @comment = @result.comments.new(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      redirect_to @result
+    else
+      render 'question#show'
+    end
   end
 
   def edit
+    @comment = Comment.find(params[:id])
   end
 
   def update
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to question_path(@comment.question.id)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to root_path
   end
 
   private
 
   def comment_params
+    params.require(:comment).permit(:content)
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
 
   def question_or_answer(params)
     if params[:answer_id]
-      @answer = Answer.find(params[:answer_id])
+      @result = Answer.find(params[:answer_id])
     else
-      @question = Question.find(params[:question_id])
+      @result = Question.find(params[:question_id])
     end
   end
-  
+
 end
