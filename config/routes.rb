4b04db4acc6cbb1884817dd0e9aece3b28d6759d
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-27 12:34:39 -0500

    Add routes. No users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  concern :commentable do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
+
+  concern :votable do
+    resources :votes, only: [:create, :destroy]
+  end
+
+  resources :question do
+    concern :commentable, :votable
+    resources :answers, only: [:create, :edit, :update, :destroy] do
+      concern :commentable, :votable
+    end
+  end
 end
