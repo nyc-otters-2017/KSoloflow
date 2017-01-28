bae26c6311162dc4387b8d0450833ca232742a36
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-27 21:30:26 -0500

    Add changes to questions controller and begin React

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,53 +2,57 @@ class QuestionsController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-    @questions = Question.all
+    questions = Question.all
+    render json: questions
   end
 
   def new
-    @question = Question.new
+    question = Question.new
+    render json: question
   end
 
   def create
-    @question = Question.new(question_params)
-    @question.user_id = current_user.id
+    question = Question.new(question_params)
+    question.user_id = current_user.id
 
-    if @question.save
-      redirect_to @question
+    if question.save
+      render json: question
     else
-      render 'new'
+      render nothing: true, status: 404
     end
   end
 
   def show
-    @question = Question.find(params[:id])
-    @answer = Answer.new
-    @comment = Comment.new
+    question = Question.find(params[:id])
+    answer = Answer.new
+    comment = Comment.new
+    render json: question
   end
 
   def edit
-    @question = Question.find(params[:id])
-    @user = @question.user
+    question = Question.find(params[:id])
+    user = question.user
+    render json: question
   end
 
   def update
-    @question = Question.find(params[:id])
-    @user = @question.user
+    question = Question.find(params[:id])
+    user = question.user
 
-    if @question.update(question_params)
-      redirect_to @question
+    if question.update(question_params)
+      render json: question
     else
-      render 'edit'
+      render json: question
     end
   end
 
   def destroy
-    @question = Question.find(params[:id])
-      @user = @question.user
-        @question.destroy
+    question = Question.find(params[:id])
+    user = question.user
+    question.destroy
 
-      redirect_to root_path
-    end
+    redirect_to root_path
+  end
 
   private
 
